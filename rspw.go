90807f800475f4fb7691de54f008b19b09754417
Matt Fonner <matt@matthewfonner.com>
2021-01-19 15:07:05 -0500

    Added support for RuneLite on Linux.

--- rspw.go
+++ rspw.go
@@ -28,6 +28,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	logger.InfoLogger.Println("Initiating retrieval from KeePass")
+	logger.InfoLogger.Println("Current databasePath value from config:", cfg.Section("config").Key("databasePath").String())
+
+	rsPass := retrievePass(cfg.Section("config").Key("databasePath").String())
+	logger.InfoLogger.Println("Data retrieved from KeePass")
+
 	// Checking if our RuneScape launcher is running
 	logger.InfoLogger.Println("Searching for RuneScape PID")
 
@@ -46,17 +52,20 @@ func main() {
 
 		logger.InfoLogger.Println("Current installPath value from config:", cfg.Section("config").Key("installPath").String())
 
-       
         // The _JAVA_OPTIONS env variable moves the files that the OSRS launcher places in /home/$USER
         // This isn't needed, but helps keeps the user's home directory nice and tidy
         // TODO: Add this to the config file
-        rs_java_env := os.Setenv("_JAVA_OPTIONS", "-Duser.home=/home/matt/.jagex/")
-        if rs_java_env != nil {
-            logger.ErrorLogger.Println("Failed to set Java environment variables before launch. Error: ", rs_java_env)
+        rsJavaEnv := os.Setenv("_JAVA_OPTIONS", "-Duser.home=/home/matt/.jagex/")
+        if rsJavaEnv != nil {
+            logger.ErrorLogger.Println("Failed to set Java environment variables before launch. Error: ", rsJavaEnv)
         }
+
 		cmd := exec.Command(cfg.Section("config").Key("installPath").String())
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+
+		// Suppressing the output of launching RuneLite
+		cmd.Stdout = nil
+		cmd.Stderr = nil
+
 		// cmd.Start() does not wait for the process to end (cmd.Run() does)
 		rs := cmd.Start()
 		if rs != nil {
@@ -71,58 +80,39 @@ func main() {
 		logger.InfoLogger.Println("Current waitTime value from config:", waitTime)
 		time.Sleep(time.Duration(waitTime) * time.Second)
 
+		robotgo.KeyTap("enter")
+		time.Sleep( 1 * time.Second)
+		robotgo.TypeStr(rsPass)
+
 		// Searching for pid again now that RuneScape is running
-		fpid, err = robotgo.FindIds("rs2client")
+		fpid, err = robotgo.FindIds("RuneLite")
 		if len(fpid) == 0 {
 			logger.ErrorLogger.Println("PID not found after launching RuneScape, exiting with error", err)
 			os.Exit(1)
 		}
 
-		logger.InfoLogger.Println("Application loaded, found RuneScape PID:", fpid)
+		//logger.InfoLogger.Println("Application loaded, found RuneScape PID:", fpid)
 	}
 
-	logger.InfoLogger.Println("RuneScape PID found:", fpid)
+	//logger.InfoLogger.Println("RuneScape PID found:", fpid)
 
 	// Grabbing the PID of the launcher
 	// This might not be needed?
-	pidExist, err := robotgo.PidExists(fpid[0])
-	if err != nil {
-		logger.ErrorLogger.Println("Error retrieving PID from ", fpid)
-	}
+	//pidExist, err := robotgo.PidExists(fpid[0])
+	//if err != nil {
+	//	logger.ErrorLogger.Println("Error retrieving PID from ", fpid)
+	//}
 
 	// Grabbing the password from our KeePass db
 	// Setting the RuneScape launcher as our active window
 	// Typing RS password into the window
-	if pidExist {
+	// TODO: Handle if RuneLite is already running
+	// Window switching isn't working yet in my testing
+	//if pidExist {
+    
+    //   switch active window here
 
-		if needsLaunched == true {
-			// Since RuneScape needed launching, we need to grab the rspw PID
-			// This will be used later to set rspw as the active window for password input
-			rspwPID, err := robotgo.FindIds("rspw")
-			if len(fpid) == 0 {
-				logger.ErrorLogger.Println("PID for rspw not found, error:", err)
-			}
-
-			logger.InfoLogger.Println("rspw PID found:", rspwPID)
-
-			// Setting rspw as the active window
-			// This handles making the user alt+tab back to rspw if RuneScape needed launching
-			robotgo.ActivePID(rspwPID[0])
-		}
-
-		logger.InfoLogger.Println("Initiating retrieval from KeePass")
-		logger.InfoLogger.Println("Current databasePath value from config:", cfg.Section("config").Key("databasePath").String())
-
-		rsPass := retrievePass(cfg.Section("config").Key("databasePath").String())
-		logger.InfoLogger.Println("Data retrieved from KeePass")
-
-		logger.InfoLogger.Println("Setting RuneScape as active window")
-		robotgo.ActivePID(fpid[0])
-
-		robotgo.TypeStr(rsPass)
-
-		logger.InfoLogger.Println("Process completed, closing rspw")
-	}
+	//}
 }
 
 func retrievePass(databasePath string) (passOut string) {
